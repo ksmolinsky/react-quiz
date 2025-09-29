7ee21a771e23a1dc95266e85fb49cd000e63c5a4
Kyle Smolinsky <kyle.smolinsky@gmail.com>
2025-09-28 20:32:54 -0400

    add start quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main"
 import Loader from "./Loader"
 import Error from "./Error"
 import StartScreen from "./StartScreen"
+import Question from "./Questions"
 
 const initialState = {
   questions: [],
@@ -25,6 +26,8 @@ function reducer(state, action) {
         ...state, 
         status: 'error',
       };
+    case 'start': 
+      return {...state, status: 'active'}
 
     default: 
       throw new Error("Action unknown");
@@ -51,7 +54,8 @@ export default function App() {
     <Main>
       {status === 'loading' && <Loader />}
       {status === "error" && <Error />}
-      {status === "ready" && <StartScreen numQuestions={numQuestions}/>}
+      {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
+      {status === "active" && <Question />}
     </Main>
   </div>
 }
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,9 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
     return (
         <div> 
             <h2>Welcome to the React Quiz!</h2>
             <p>{numQuestions} questions to test your React mastery</p>
-            <button className="btn btn-ui">Let's start</button>
+            <button className="btn btn-ui" onClick={() => dispatch({ type: "start" })}>Let's start</button>
         </div>
     )
 }
